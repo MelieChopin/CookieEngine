489c2bc10c46d79ba83bcac1275bac8942a817a5
Hugo Chemouny <h.chemouny@student.isartdigital.com>
2021-05-12 12:22:44 +0200

    Playflow is now fully operational (pause and frame added) from the Toolbar
    Stable
    
    UI - Toolbar
    
    Playflow is now fully operational (pause and frame added) from the Toolbar

--- CookieEngine/include/UI/Widgets/EditorWs/ToolbarWidget.hpp
+++ CookieEngine/include/UI/Widgets/EditorWs/ToolbarWidget.hpp
@@ -21,13 +21,26 @@ namespace Cookie::UIwidget
 		std::unique_ptr<Cookie::Resources::Texture> icons[(int)ToolbarIcons::ICOS_NUM];
 		
 		TransformTool	currentTrsfTool	= TransformTool::Translate;
-		bool&			playing;
+
+		
+		bool& isPlaying;
+
+		bool playing	= false;
+		bool paused		= false;
+		bool frame		= false;
+
+	private:
+		// Displays all the available tools and playflow options, can be interacted with at all times of course.
+		void ButtonsDisplay();
+
+		// Based on the 3 playflow booleans, the return determines the frame value of "isPlaying".
+		bool ProcessPlayFlow();
 
 	protected:
 		bool BeginWindow(int windowFlags = 0) override;
 
 	public:
-		Toolbar(Cookie::Render::Renderer& _renderer, bool& _playing);
+		Toolbar(Cookie::Render::Renderer& _renderer, bool& _isPlaying);
 
 		void WindowDisplay() override;
 
--- CookieEngine/src/UI/Widgets/EditorWs/ToolbarWidget.cpp
+++ CookieEngine/src/UI/Widgets/EditorWs/ToolbarWidget.cpp
@@ -8,9 +8,9 @@ using namespace ImGui;
 using namespace Cookie::UIwidget;
 
 
-Toolbar::Toolbar(Cookie::Render::Renderer& _renderer, bool& _playing)
+Toolbar::Toolbar(Cookie::Render::Renderer& _renderer, bool& _isPlaying)
 	: WindowBase	("##TOOLBAR"),
-	  playing		(_playing)
+	  isPlaying		(_isPlaying)
 {
 	icons[(int)ToolbarIcons::Translator]	= std::make_unique<Cookie::Resources::Texture>("Assets/EditorUIcons/translate.ico");
 	icons[(int)ToolbarIcons::Rotator]		= std::make_unique<Cookie::Resources::Texture>("Assets/EditorUIcons/rotate.ico");
@@ -30,55 +30,97 @@ bool Toolbar::BeginWindow(int windowFlags)
 	return true;
 }
 
-void Toolbar::WindowDisplay()
+void Toolbar::ButtonsDisplay()
 {
 	PushStyleColor(ImGuiCol_Button,			{ 0.70, 0.70, 0.70, 1 });
 	PushStyleColor(ImGuiCol_ButtonActive,	{ 0.35, 0.35, 0.35, 1 });
 	PushStyleColor(ImGuiCol_ButtonHovered,	{ 0.90, 0.90, 0.90, 1 });
+	
+	PushStyleVar(ImGuiStyleVar_FrameRounding, 5.f);
 
 
+	SameLine(15);
+
+	if (ImageButton(icons[(int)ToolbarIcons::Translator]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		currentTrsfTool = TransformTool::Translate;
+	} 
+	SameLine(15 + (35 * 1));
+
+	if (ImageButton(icons[(int)ToolbarIcons::Rotator]	->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		currentTrsfTool = TransformTool::Rotate;
+	}	
+	SameLine(15 + (35 * 2));
+
+	if (ImageButton(icons[(int)ToolbarIcons::Scaler]	->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		currentTrsfTool = TransformTool::Scale;
+	}		
+	SameLine(15 + (35 * 3));
+
+
+	SameLine(GetWindowWidth() / 2);
+
+	if		(!playing && ImageButton(icons[(int)ToolbarIcons::Play]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		playing = true;
+	}
+	else if ( playing && ImageButton(icons[(int)ToolbarIcons::Stop]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		playing = false;
+	}
+	SameLine(GetWindowWidth() / 2 + (35 * 1));
+
+
+	if (ImageButton(icons[(int)ToolbarIcons::Pause]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		paused ^= 1;
+	}
+	SameLine(GetWindowWidth() / 2 + (35 * 2));
+
+
+	if (ImageButton(icons[(int)ToolbarIcons::Frame]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
+	{
+		if (!paused) paused = true;
+
+		frame = true;
+	}
+
+
+	PopStyleVar(1);
+	PopStyleColor(3);
+}
+
+
+bool Toolbar::ProcessPlayFlow()
+{
+	if (playing)
+	{
+		if (paused)
+		{
+			if (frame)
+			{
+				frame = false;
+				return true;
+			}
+			else return false;
+		}
+		else return true;
+	}
+	else return false;
+}
+
+
+void Toolbar::WindowDisplay()
+{
 	TryBeginWindow(ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar)
 	{
-		SameLine(15);
-
-		PushStyleVar(ImGuiStyleVar_FrameRounding, 5.f);
-
-		if (ImageButton(icons[(int)ToolbarIcons::Translator]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{ currentTrsfTool = TransformTool::Translate; } SameLine(15 +(35*1));
-
-		if (ImageButton(icons[(int)ToolbarIcons::Rotator]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{ currentTrsfTool = TransformTool::Rotate; }	SameLine(15 +(35*2));
-
-		if (ImageButton(icons[(int)ToolbarIcons::Scaler]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{ currentTrsfTool = TransformTool::Scale; }		SameLine(15 +(35*3));
-		
-
-		SameLine(GetWindowWidth()/2);
-
-		if		(!playing && ImageButton(icons[(int)ToolbarIcons::Play]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{
-			playing = true; 
-			/*TBD*/
-		}
-		else if (playing && ImageButton(icons[(int)ToolbarIcons::Stop]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{
-			playing = false; 
-			/*TBD*/
-		}	
-		
-		SameLine(GetWindowWidth()/2 +(35*1));
-
-
-		if (ImageButton(icons[(int)ToolbarIcons::Pause]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{/*TBD*/}	SameLine(GetWindowWidth()/2 +(35*2));
-
-		if (ImageButton(icons[(int)ToolbarIcons::Frame]->GetResourceView(), { 25, 25 }, { 0, 0 }, { 1, 1 }, 5))
-		{/*TBD*/}
-		
-		PopStyleVar(1);
+		ButtonsDisplay();
 	}
 
 	ImGui::EndChild();
 
-	PopStyleColor(3);
+	
+	isPlaying = ProcessPlayFlow();
 }
